548df3f32df32b2a664e956ad8717c457900a12d
sirajali <sirajalipyt@gmail.com>
2024-02-28 19:17:47 +0500

    Beta Number Guessing Game

---
+++ py1.py
@@ -0,0 +1,26 @@
+import random
+
+print("tutorial timee!!!")
+
+tries = 0
+range1 = int(input("whats the lower num? "))
+range2 = int(input("whats the higher num? "))
+randomnum = random.randint(range1,range2)
+
+
+while True:
+    try:
+        answer = int(input("game time!, what is the chosen num?"))
+        tries += 1 
+        if answer > randomnum:
+            print("wrong!!, your guess was higher")
+        if answer < randomnum:
+            print("wrong!!, your guess was lower")
+        if answer == randomnum:
+            print("Correct!, {} was the right number it took {} tries".format(randomnum,tries))
+            break
+        
+    except ValueError:
+        print("NOPE")
+
+
